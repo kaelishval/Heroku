a620fdb852fd65387092ba935ec29db021684297
kaelishval <kaelishvali@gmail.com>
2021-11-16 16:20:24 +0800

    testteee

---
+++ delete.php
@@ -0,0 +1,12 @@
+<?php include ('config/db.php')?>
+<?php include ('config/config.php')?>
+<?php
+ if(isset($_GET['userid'])){
+     $user_id=$_GET['userid'];
+
+$sql = "DELETE FROM users WHERE 'id'='$user_id'";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();    
+  
+ }
+ ?>
