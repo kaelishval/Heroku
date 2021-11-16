efc0def2805492321d2812da14c87e2763806e57
kaelishval <kaelishvali@gmail.com>
2021-11-16 13:52:35 +0800

    dawdaaaaadsadddfa

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 <?php include ('config/config.php')?>
 
 <?php
-   if (isset($_POST['submit'])) {
+   if ($_POST['submit']=="save"){
 
        $first_name = $_POST['firstname'];
        $last_name = $_POST['lastname'];
        $email = $_POST['email'];
        $gender =  $_POST['gender'];
    }
-   $sql = "INSERT INTO users (firstname,lastname,email,gender) VALUES ('$first_name','$last_name','$email','$gender')";
-   
+   $sql = "INSERT INTO users (firstname,lastname,email,gender) VALUES ($first_name,$last_name,$email,$gender)";
+   console.log($sql);
 ?>
 
 <html>
@@ -33,7 +33,7 @@
         Gender:<br />
         <input type="radio" name="gender" value="male" />Male
         <input type="radio" name="gender" value="female" />Female <br /><br />
-        <input type="submit" name="submit" value="submit" />
+        <input type="submit" name="submit" value="save" />
       </fieldset>
     </form>
   </body>
