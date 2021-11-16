e2659b5efb277b08b5aedb74a411e78b69462065
kaelishval <kaelishvali@gmail.com>
2021-11-16 11:02:47 +0800

    update

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
+<?php include ('config/db.php')?>
+<?php include ('config/config.php')?>
+
 <?php
 
- echo "Hello there , HEROKU!!!"
+ echo "Hello there , HEROKU!!!";
+
+ 
+ //echo 'This is Index Page';
+
+ $sql = 'SELECT * FROM users';
+ $stmt = $pdo->prepare($sql);
+ $stmt->execute();
+ $rowCount = $stmt->rowCount();
+ $details = $stmt->fetch();
+
+ print_r ($details);
+
 
 ?>
