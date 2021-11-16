7ecfaa47bb52e85b2767633ab8071e43bf9aa4bd
kaelishval <kaelishvali@gmail.com>
2021-11-16 16:32:49 +0800

    testteee99

--- delete.php
+++ delete.php
@@ -1,7 +1,7 @@
 <?php include ('config/db.php')?>
 <?php include ('config/config.php')?>
 <?php
- if(isset($_GET['userid'])){
+ if($_GET['userid']!==NULL){
      $user_id = $_GET['userid'];
 
 $sql = "DELETE FROM 'users' WHERE 'userid'='$user_id'";
