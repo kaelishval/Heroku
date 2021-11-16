c6841d2503241649d76a2937c08fda4172b3351f
kaelishval <kaelishvali@gmail.com>
2021-11-16 14:33:29 +0800

    ddf

--- index.php
+++ index.php
@@ -3,25 +3,16 @@
 
 
    <?php
-   //echo 'This is Index Page';
+   echo 'This is Index Page';
  
-  //  $sql = 'SELECT * FROM users';
-  //  $stmt = $pdo->prepare($sql);
-  //  $stmt->execute();
-  //  $rowCount = $stmt->rowCount();
-  //  $details = $stmt->fetch();
+   $sql = 'SELECT * FROM users';
+   $stmt = $pdo->prepare($sql);
+   $stmt->execute();
+   $rowCount = $stmt->rowCount();
+   $details = $stmt->fetch();
   
-  //  print_r ($details);
-  if ($_POST['submit']=="save") {
-
-    $first_name = $_POST['firstname'];
-    $last_name = $_POST['lastname'];
-    $email = $_POST['email'];
-    $gender =  $_POST['gender'];
-}
-    $sql = $conn->prepare("INSERT INTO users (firstname,lastname,email,gender) VALUES ('$first_name','$last_name','$email','$gender')");
-    $sql->execute();
-
+   print_r ($details);
+ 
    
  ?>
 
