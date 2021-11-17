8d52ec7b53ffbdb5d3a355ad8ad7dd1fbf5e506d
kaelishval <kaelishvali@gmail.com>
2021-11-18 07:52:32 +0800

    laterst

--- update.php
+++ update.php
@@ -7,9 +7,37 @@ session_start();
 
 $regValue = $_GET['regName'];
 
+if ($_POST['submit']=='save') {
 
-echo $regValue;
+}
 
 ?>
 
 <a class="nav-link" href="view.php">BACK</a>
+
+<html>
+  <body>
+    <h2>CREATE</h2>
+    <form action="" method="POST">
+      <fieldset>
+        <legend>Insert Info</legend>
+        User ID:<br />
+        <input type="text" name="userid" />
+        <br />
+        First Name:<br />
+        <input type="text" name="firstname" />
+        <br />
+        Last Name:<br />
+        <input type="text" name="lastname" />
+        <br />
+        email:<br />
+        <input type="text" name="email" />
+        <br />
+        Gender:<br />
+        <input type="radio" name="gender" value="male" />Male
+        <input type="radio" name="gender" value="female" />Female <br /><br />
+        <input type="submit" name="submit" value="save" />
+      </fieldset>
+    </form>
+  </body>
+</html>
