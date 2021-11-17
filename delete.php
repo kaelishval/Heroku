56bcc2b55e96a048a7e5723b42b9b6219fcc553a
kaelishval <kaelishvali@gmail.com>
2021-11-18 07:57:48 +0800

    laterst

--- delete.php
+++ delete.php
@@ -12,7 +12,7 @@ $sql = "DELETE FROM users WHERE userid='$regValue'";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();  
 echo "Entry DELETED";
-
+header("Location: view.php"); 
 ?>
 
 <a class="nav-link" href="view.php">BACK</a>
