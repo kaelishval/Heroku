26b77a732ae8763146ea8755297cc35e7c0a2063
kaelishval <kaelishvali@gmail.com>
2021-11-24 07:14:58 +0800

    awdawd

--- test.php
+++ test.php
@@ -1,36 +1,55 @@
+<?php include ('config/db.php')?>
+<?php include ('config/config.php')?>
 <?php
 class test {
 
 private $str = NULL;
 
-public function newTest($user_id, $first_name,$last_name,$email,$gender){
+public function create($tableName , array $baseStrings){
 
     
-   $sql = "INSERT INTO users (userid,firstname,lastname,email,gender) VALUES ('$user_id','$first_name','$last_name','$email','$gender')";
+   $sql = "INSERT INTO $tableName (userid,firstname,lastname,email,gender) VALUES ('$baseStrings[0]','$baseStrings[0]','$baseStrings[0]','$baseStrings[0]','$baseStrings[0]')";
    $stmt = $pdo->prepare($sql);
    $stmt->execute();
-   $rowCount = $stmt->rowCount();
-   $details = $stmt->fetch();
   
    print_r ($sql);
+
+   if($stmt->execute()){
+
+    echo "Success";
+
+    }
+
+    echo "Error";
     
 }
-public function bigTest(){
+public function update($tableName,array $baseStrings, $ids){
 
-    return $this->str . ' function "bigTest" called,';
+    $sql = "UPDATE $tableName SET firstname='$baseStrings[0]',lastname='$baseStrings[1]',email='$baseStrings[2]',gender='$baseStrings[3]' WHERE id='$ids'";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+   
+    print_r ($sql);
+    // echo "$tableName";
+    // echo "$baseStrings[0],$baseStrings[1],$baseStrings[2],$baseStrings[3]";
+    // echo "$ids";
 }
-public function smallTest(){
+public function delete($tableName,$ids){
 
-    return $this->str . ' function "smallTest" called,';
-}
-public function scoreTest(){
+    $sql = "DELETE FROM $tableName WHERE id='$ids'";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();  
 
-    return $this->str . ' function "scoreTest" called,';
+    print_r ($sql);
 }
 }
 
 $test = new test;
 
-$test->newTest('1','test1','test1','test1','test1');
+$test->create('users',array('test1','test1','test1','test1'));
+
+// $test->update('users',array('test2','test3','test2','test2'),1);
+
+// $test->delete('users','1');
 
 ?>
