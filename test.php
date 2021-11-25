c2d0ebec3b3982c07a8592f393b5b26152fb3dfb
kaelishval <kaelishvali@gmail.com>
2021-11-25 11:56:39 +0800

    adawdawdawdawd

--- test.php
+++ test.php
@@ -1,6 +1,32 @@
 <?php
-include ('config/db.php');
-include ('config/config.php');
+  define('ROOT_URL','');
+?>
+<?php
+
+$host = "ec2-18-235-154-252.compute-1.amazonaws.com";
+$user = "ajpiganlxdmpab";
+$password = "f57de192a79b0d042533cd7772a5ac6e66d70bf6cb4c7b18acac64b26a2e03e2";
+$dbname = "db4vttbnb1juur";
+$port = "5432";
+
+try{
+  //Set DSN data source name
+    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+
+
+  //create a pdo instance
+  $pdo = new PDO($dsn, $user, $password);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+echo 'Connection failed: ' . $e->getMessage();
+}
+?>
+<?php
+
+
 class test {
 
 public function create($tableName , array $baseStrings){
