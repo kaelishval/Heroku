8364be68e951566ca081714b1e52eeaae8ab0a39
kaelishval <kaelishvali@gmail.com>
2021-11-25 12:38:47 +0800

    adawdawdawdawd

--- test.php
+++ test.php
@@ -15,23 +15,25 @@ public function create($pdo,$tableName , array $baseStrings){
 public function update($pdo,$tableName,array $baseStrings, $ids){
 
     $sql = "UPDATE $tableName SET id='$user_id',first_name__c='$first_name',last_name__c='$last_name',email__c='$email',gender__c='$gender' WHERE id='$ids'";
-    $pdo->query($sql);
+    $stmt = $pdo->query($sql);
+    print_r($stmt); 
     
   
 }
 public function delete($pdo,$tableName,$ids){
 
     $sql = "DELETE FROM $tableName WHERE id='$ids'";
-    $pdo->query($sql);
+    $stmt = $pdo->query($sql);
+    print_r($stmt); 
 
 }
 }
 print("<br><br> CUD class test:<br>------------------------<br>");
 $tests = new test;
 print("<br>CREATE:<br><br>");
-$tests->create($pdo,'Salesforce.uzer__c',array('kael','mauro','last@gmail.com','male'));
+// $tests->create($pdo,'Salesforce.uzer__c',array('kael','mauro','last@gmail.com','male'));
 print("<br>UPDATE:<br><br>");
-// $tests->update('Salesforce.uzer__c',array('test2','test3','test2','test2'),1);
+$tests->update($pdo,'Salesforce.uzer__c',array('test2','test3','test2','test2'),1461);
 print("<br>DELETE:<br><br>");
 // $tests->delete('Salesforce.uzer__c','1');
 ?>
