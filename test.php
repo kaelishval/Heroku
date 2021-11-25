df69bd01e1ca6490c2db11126b363a960584c16d
kaelishval <kaelishvali@gmail.com>
2021-11-25 10:39:08 +0800

    awdawdawd

--- test.php
+++ test.php
@@ -1,3 +1,5 @@
+<?php include ('config/db.php')?>
+<?php include ('config/config.php')?>
 <?php
 
 
@@ -7,10 +9,10 @@ public function create($tableName , array $baseStrings){
 
    $user_id = rand(10,10000);
     
-   $sql = "INSERT INTO $tableName (userid,firstname,lastname,email,gender) VALUES ('$user_id','$baseStrings[0]','$baseStrings[1]','$baseStrings[2]','$baseStrings[3]')";
+   $sql = "INSERT INTO $tableName (id,first_name__c,last_name__c,email__c,gender__c) VALUES ('$user_id','$baseStrings[0]','$baseStrings[1]','$baseStrings[2]','$baseStrings[3]')";
    $stmt = $pdo->query($sql);
    print_r ($sql);
-
+   
 //    if($stmt->execute()){
 //     $ids = $user_id;
 //     return $ids;
@@ -35,7 +37,7 @@ public function delete($tableName,$ids){
 
 $test = new test;
 
-// $test->create('users',array('kael','mauro','last@gmail.com','male'));
+$test->create('Salesforce.uzer__c',array('kael','mauro','last@gmail.com','male'));
 
 // $test->update('users',array('test2','test3','test2','test2'),1);
 
