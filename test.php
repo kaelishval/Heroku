64ae5d75524853a1494bd5bdd530e975208a2e45
kaelishval <kaelishvali@gmail.com>
2021-11-25 11:23:02 +0800

    awdawdawdawdaw

--- test.php
+++ test.php
@@ -8,7 +8,7 @@ public function create($tableName , array $baseStrings){
    $user_id = rand(10,10000);
     
    $sql = "INSERT INTO $tableName (id,first_name__c,last_name__c,email__c,gender__c) VALUES ('$user_id','$baseStrings[0]','$baseStrings[1]','$baseStrings[2]','$baseStrings[3]')";
-   
+   $stmt = $pdo->query($sql);
    print_r ($sql);
    
 //    if($stmt->execute()){
