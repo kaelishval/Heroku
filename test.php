aca2979736d1a741f96ea6feeaa21b877aec61d3
kaelishval <kaelishvali@gmail.com>
2021-11-25 13:13:03 +0800

    adawdawdawdawd

--- test.php
+++ test.php
@@ -32,7 +32,7 @@ public function testStartTime($pdo)
   $sql = "SELECT * FROM salesforce._trigger_log ORDER BY id DESC LIMIT 1";
   $stmt = $pdo->query($sql);
   while (($row = $stmt->fetch(PDO::FETCH_ASSOC)) !== false) {
-    return print("Action: " . $row["action"] . " || " . $row["processed_at"]);
+    return print("Action: " . $row["action"] . " || " . $row["processed_at"]." || " . $row["created_at"]);
   }
 }
 public function logResults($pdo)
